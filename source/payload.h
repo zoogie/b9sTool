1bfc1bdf6e4318c8e5a54205976d76eb8234184c
zoogie <secretAgentCokey@gmail.com>
2019-08-29 19:16:08 -0500

    11.11 changes

--- source/payload.h
+++ source/payload.h
@@ -1,3 +1,3 @@
-const char firmwareNames[] = "For firmware 11.10.0-X ONLY";
+const char firmwareNames[] = "For 11.10.0-X or 11.11.0-X ONLY";
 const unsigned int payload_len = 30720;
 extern unsigned char payload[];
